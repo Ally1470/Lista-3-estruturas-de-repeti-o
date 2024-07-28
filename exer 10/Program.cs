0b65e6152e9340ed846c52d84525ca6c5d1bf991
Ally1470 <166332838+Ally1470@users.noreply.github.com>
2024-07-27 23:43:07 -0300

    Add files via upload

---
+++ exer 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Não consegui fazer do exercico 10, 11, 12, 13");
+        }
+    }
+}
